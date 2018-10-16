4857b893e06dd9b150545864a608463f5b598172
Roman <Roman_Selianko@epam.com>
2018-10-17 02:03:55 +0400

    Added first task

--- jvm-memory/java/second_homework/out_of_memory/First_Task.java
+++ jvm-memory/java/second_homework/out_of_memory/First_Task.java
@@ -5,12 +5,36 @@ import java.util.List;
 
 public class First_Task {
     public static void main(String... args) {
+
         //throws an outOfMemoryError: Java heap space exception
         List<Integer> crashedList = new ArrayList<>();
-        while (true)
-            crashedList.add(1);
+       /* while (true)
+            crashedList.add(1);*/
 
+        //throws an outOfMemoryError: Metaspace
+        //-XX:MetaspaceSize=1M -XX:MaxMetaspaceSize=10M
+        ClassLoader classLoader = First_Task.class.getClassLoader();
+        ArrayList strangeList = new ArrayList();
+        try {
+            while (true) {
+                Class aClass = classLoader.loadClass("second_homework.out_of_memory.First_Task");
+                try {
+                    strangeList.add(aClass.newInstance());
+                } catch (InstantiationException | IllegalAccessException e) {
+                    e.printStackTrace();
+                }
+                //  System.out.println("aClass.getName() = " + aClass.getName());
+            }
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+        //StackOverFlowError with recursion
+        recursion();
+        //one way to throw StackOverFlowError without recursion
+        throw new StackOverflowError();
+    }
 
-
+    public static void recursion() {
+        recursion();
     }
 }
