5c25b3abc8fb5ea43ded73e2112c0eb3da29eb27
Roman Selianko <romanselyanko@gmail.com>
2019-01-28 12:06:20 +0400

    Initial commit

---
+++ jdbc-advanced/src/main/java/com/epam/homework/jdbcadvanced/JdbcAdvancedApplication.java
@@ -0,0 +1,14 @@
+package com.epam.homework.jdbcadvanced;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JdbcAdvancedApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JdbcAdvancedApplication.class, args);
+    }
+
+}
+
