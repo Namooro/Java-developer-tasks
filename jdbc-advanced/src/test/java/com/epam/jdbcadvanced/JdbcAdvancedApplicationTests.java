b0ba67f5cc49cba7fe6cff67787d393566f50bb0
Roman Selianko <romanselyanko@gmail.com>
2019-01-30 14:36:04 +0400

    Test fails but named properly

--- jdbc-advanced/src/test/java/com/epam/jdbcadvanced/JdbcAdvancedApplicationTests.java
+++ jdbc-advanced/src/test/java/com/epam/jdbcadvanced/JdbcAdvancedApplicationTests.java
@@ -16,7 +16,6 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import java.time.LocalDate;
@@ -29,7 +28,6 @@ import static org.junit.Assert.assertEquals;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
 public class JdbcAdvancedApplicationTests {
 
     @Autowired
