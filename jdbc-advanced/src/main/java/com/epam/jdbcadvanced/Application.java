cb83006abf467490f48e9693cb7b708dcff63851
Roman Selianko <romanselyanko@gmail.com>
2019-02-15 21:31:22 +0400

    cleanup

--- jdbc-advanced/src/main/java/com/epam/jdbcadvanced/Application.java
+++ jdbc-advanced/src/main/java/com/epam/jdbcadvanced/Application.java
@@ -1,20 +1,16 @@
 package com.epam.jdbcadvanced;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.jdbc.core.JdbcTemplate;
 
 public class Application implements CommandLineRunner {
 
-    private static final Logger log = LoggerFactory.getLogger(Application.class);
-
     @Autowired
     JdbcTemplate jdbcTemplate;
 
     @Override
-    public void run(String... args) throws Exception {
-//jdbcTemplate.batchUpdate()
+    public void run(String... args) {
+
     }
 }
